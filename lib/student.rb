e3ddd5b24e0bcaed897c0c2ba2810e7c7ec3fb79
Alice Brunel <alc.brunel@gmail.com>
2019-09-17 15:26:38 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -14,6 +14,14 @@ class Student
   end
   
   def create_table
-    
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      )
+    SQL
+
+    DB[:conn].execute(sql)
   end
 end
