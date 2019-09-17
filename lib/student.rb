3d86d4376429c44556df45c082dc960662e65224
Alice Brunel <alc.brunel@gmail.com>
2019-09-17 15:40:06 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,8 +46,8 @@ class Student
   end
   
   def self.update
-sql = "UPDATE students SET name = ?, album = ? WHERE name = ?"
-    DB[:conn].execute(sql, self.name, self.album, self.name)
+sql = "UPDATE students SET name = ?, grade = ? WHERE name = ?"
+    DB[:conn].execute(sql, self.name, self.grade, self.name)
   end
   
 end
